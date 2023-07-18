1ed05d7b16ef0e11f819142f726b8438d1834b33
Mueem-Nahid <mueem51@gmail.com>
2023-07-18 22:54:01 +0600

    # Fix - fix in navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,22 +1,6 @@
-import {
-   Box,
-   Burger,
-   Button,
-   Collapse,
-   createStyles,
-   Divider,
-   Drawer,
-   Group,
-   Header,
-   rem,
-   ScrollArea,
-   Text,
-   ThemeIcon,
-   UnstyledButton,
-} from '@mantine/core';
+import {Box, Burger, Button, createStyles, Divider, Drawer, Group, Header, rem, ScrollArea,} from '@mantine/core';
 import {MantineLogo} from '@mantine/ds';
 import {useDisclosure} from '@mantine/hooks';
-import {IconBook, IconChartPie3, IconCode, IconCoin, IconFingerprint, IconNotification,} from '@tabler/icons-react';
 import {Link} from "react-router-dom";
 
 const useStyles = createStyles((theme) => ({
@@ -79,61 +63,9 @@ const useStyles = createStyles((theme) => ({
    },
 }));
 
-const mockdata = [
-   {
-      icon: IconCode,
-      title: 'Open source',
-      description: 'This Pokémon’s cry is very loud and distracting',
-   },
-   {
-      icon: IconCoin,
-      title: 'Free for everyone',
-      description: 'The fluid of Smeargle’s tail secretions changes',
-   },
-   {
-      icon: IconBook,
-      title: 'Documentation',
-      description: 'Yanma is capable of seeing 360 degrees without',
-   },
-   {
-      icon: IconFingerprint,
-      title: 'Security',
-      description: 'The shell’s rounded shape and the grooves on its.',
-   },
-   {
-      icon: IconChartPie3,
-      title: 'Analytics',
-      description: 'This Pokémon uses its flying ability to quickly chase',
-   },
-   {
-      icon: IconNotification,
-      title: 'Notifications',
-      description: 'Combusken battles with the intensely hot flames it spews',
-   },
-];
-
 export function Navbar() {
    const [drawerOpened, {toggle: toggleDrawer, close: closeDrawer}] = useDisclosure(false);
-   const [linksOpened, {toggle: toggleLinks}] = useDisclosure(false);
    const {classes, theme} = useStyles();
-
-   const links = mockdata.map((item) => (
-      <UnstyledButton className={classes.subLink} key={item.title}>
-         <Group noWrap align="flex-start">
-            <ThemeIcon size={34} variant="default" radius="md">
-               <item.icon size={rem(22)} color={theme.fn.primaryColor()}/>
-            </ThemeIcon>
-            <div>
-               <Text size="sm" fw={500}>
-                  {item.title}
-               </Text>
-               <Text size="xs" color="dimmed">
-                  {item.description}
-               </Text>
-            </div>
-         </Group>
-      </UnstyledButton>
-   ));
 
    return (
       <Box>
@@ -168,23 +100,23 @@ export function Navbar() {
             onClose={closeDrawer}
             size="100%"
             padding="md"
-            title="Navigation"
+            title="Book Self"
             className={classes.hiddenDesktop}
             zIndex={1000000}
          >
             <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
                <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}/>
-
                <Link to='/' className={classes.link}>
                   Home
                </Link>
-               <Collapse in={linksOpened}>{links}</Collapse>
-
                <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}/>
-
                <Group position="center" grow pb="xl" px="md">
-                  <Button variant="default">Log in</Button>
-                  <Button>Sign up</Button>
+                  <Link to='/login'>
+                     <Button variant="default">Log in</Button>
+                  </Link>
+                  <Link to='/signup'>
+                     <Button>Sign up</Button>
+                  </Link>
                </Group>
             </ScrollArea>
          </Drawer>
