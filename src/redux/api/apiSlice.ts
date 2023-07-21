bb5dc66e901d570aea7e547099670437fa573b28
Mueem-Nahid <mueem51@gmail.com>
2023-07-22 00:50:29 +0600

    # Info - .env added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const api = createApi({
    reducerPath: 'api',
    baseQuery: fetchBaseQuery({
-      baseUrl: 'http://localhost:8080/api/v1',
+      baseUrl: import.meta.env.VITE_BASE_API_URL,
       prepareHeaders: (headers, {getState}) => {
          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
          // @ts-ignore
